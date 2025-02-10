0cad25cb21a405b0c2d3802c29fa1dbd3670fb56
lockiultra <lockiultra@gmail.com>
2025-02-10 11:50:00 +0300

    Security update

--- config/keys.py
+++ config/keys.py
@@ -2,4 +2,4 @@ import os
 
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-TOGETHER_API_KEY = '0bed3036afb847f3c77fbf74363fe0beeae72a81ba3e5716e1b6ea7059b97b66'
+TOGETHER_API_KEY = os.environ.get('TOGETHER_API_KEY')
